88b3ab800f1d19aaea4d158c8efa9987266377b5
Marcus Macauley <50225950+macauleydev@users.noreply.github.com>
2023-09-02 15:32:53 -0400

    Do exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let reversedString = "";
+    for (let index = string.length - 1; index >= 0; index--) {
+        reversedString += string.slice(index, index + 1);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
