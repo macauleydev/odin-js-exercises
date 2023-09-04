00455f47611cbefb475a95acbb37d4c8405d18ca
Marcus Macauley <50225950+macauleydev@users.noreply.github.com>
2023-09-04 09:32:51 -0400

    Do exercise 6 (leapYears)

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,26 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    // Divisble by 400? Leap year. Else...
+    // Divisble by 100? Not a leap year. Else...
+    // Divisible by 4? Leap year. Else...
+    // Not a leap year.
+    let isLeapYear;
 
+    isLeapYear =
+        year % 400 == 0
+            ? true
+            : year % 100 == 0
+            ? false
+            : year % 4 == 0
+            ? true
+            : false;
+    return isLeapYear;
 };
+
+// Alternatively:
+// Not a leap year if:
+// - Divisble by 100 but not by 400
+// - Not divisible by 4
+// Otherwise a leap year
 
 // Do not edit below this line
 module.exports = leapYears;
