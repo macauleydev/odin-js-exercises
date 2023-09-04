58f3feac4d58d8d54a28a0ecd126ce43d60b53e2
Marcus Macauley <50225950+macauleydev@users.noreply.github.com>
2023-09-04 09:49:49 -0400

    Do exercise 7 (tempConversion)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempF) {
+    let exactTempC = ((tempF - 32) * 5) / 9;
+    let approxTempC = Math.round(exactTempC * 10) / 10;
+    return approxTempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+    let exactTempF = (tempC * 9) / 5 + 32;
+    let approxTempF = Math.round(exactTempF * 10) / 10;
+    return approxTempF;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
