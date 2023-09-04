e6fa8c1b696ddc9e8fc2a149372338671dfe3f84
Marcus Macauley <50225950+macauleydev@users.noreply.github.com>
2023-09-04 09:23:54 -0400

    Do exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    if (num1 !== parseInt(num1) || num2 !== parseInt(num2)) {
+        return "ERROR";
+    }
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    let smallerNum, largerNum;
+    if (num1 <= num2) {
+        smallerNum = num1;
+        largerNum = num2;
+    } else {
+        largerNum = num1;
+        smallerNum = num2;
+    }
+    let sum = 0;
+    for (let i = smallerNum; i <= largerNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
