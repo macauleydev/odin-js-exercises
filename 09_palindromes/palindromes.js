00a4bdfe767a045f1d3db85f62b23ea257e417ac
Marcus Macauley <50225950+macauleydev@users.noreply.github.com>
2023-09-15 14:20:10 -0400

    Do exercise 9 (palindromes)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = alphaNumeric(string).toLowerCase();
+    let right = string.length - 1;
+    console.log(string);
+    for (let left = 0; left < right; left++) {
+        if (string[left] !== string[right]) {
+            return false;
+        }
+        right--;
+    }
+    return true;
 };
+
+const isLetter = (string) => string.toLowerCase() !== string.toUpperCase();
+const isNumber = (string) => string == +string;
+
+function alphaNumeric(string) {
+    let result = "";
+    for (let char of string) {
+        
+        if (char !== " " && (isLetter(char) || isNumber(char))) result += char;
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
