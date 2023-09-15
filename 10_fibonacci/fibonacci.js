d575c819dc80dd49789b294f307c1305b52bcedf
Marcus Macauley <50225950+macauleydev@users.noreply.github.com>
2023-09-15 14:45:51 -0400

    Do exercise 10 (Fibonacci)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (member) {
+    if (member < 0 || member != parseInt(member)) return "OOPS";
+    let fibonacciArray = [];
+    for (let i = 0; i < member; i++) {
+        let index1 = i - 2;
+        let index2 = i - 1;
+        let member1 = index1 < 0 ? 0 : fibonacciArray[index1];
+        let member2 = index2 < 0 ? 1 : fibonacciArray[index2];
+        fibonacciArray[i] = member1 + member2;
+    }
+    return fibonacciArray[member - 1];
 };
 
 // Do not edit below this line
