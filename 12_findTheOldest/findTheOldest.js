0a2d32c02c085abfba6540b72866d15eb9bd37bb
Marcus Macauley <50225950+macauleydev@users.noreply.github.com>
2023-09-15 15:26:11 -0400

    Do exercise 12 (Find the oldest)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    let currentYear = new Date().getFullYear();
+    let ages = peopleArray.map(person => (person.yearOfDeath ?? currentYear) - person.yearOfBirth);
+    let indexOfMaxAge = ages.findIndex(age => age === Math.max(...ages));
+    return peopleArray[indexOfMaxAge];
 };
 
 // Do not edit below this line
